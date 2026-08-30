e52bd7e6d01dee4158271da048d85f8306aec7f9
doodler132 <yarikarutynyan86@gmail.com>
2026-08-30 19:47:56 +0300

    Update Supabase configuration with new keys

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // The publishable/anon key is designed to be used in browser apps.
 // NEVER put a service_role/secret key here.
 window.PORTFOLIO_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_PUBLISHABLE_KEY: '',
+  SUPABASE_URL: 'https://fdnlcnqohhcjmjcawsvk.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_U0y3u4SW6czjUvY8YJCevQ_prYFInqB',
   STORAGE_BUCKET: 'portfolio-media'
 };
